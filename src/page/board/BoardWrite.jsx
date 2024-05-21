3e255325797eed40a419c69d4f479db3905bd7d4
1dragon <dnjsdyd159@naver.com>
2024-05-21 12:20:09 +0900

    글 등록 성공시 toast 출력, HOME으로 이동

--- src/page/board/BoardWrite.jsx
+++ src/page/board/BoardWrite.jsx
@@ -5,22 +5,39 @@ import {
   FormLabel,
   Input,
   Textarea,
+  useToast,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function BoardWrite() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [writer, setWriter] = useState("");
+  const toast = useToast();
+  const navigate = useNavigate();
 
   function handleSaveClick() {
-    axios.post("/api/board/add", {
-      /* 이름이 같으면 생략 가능 */
-      title,
-      content,
-      writer,
-    });
+    axios
+      .post("/api/board/add", {
+        /* 이름이 같으면 생략 가능 */
+        title,
+        content,
+        writer,
+      })
+      .then(() => {
+        /* 새 글을 등록 성공시 알림창 */
+        toast({
+          description: "새 글이 등록되었습니다.",
+          status: "success",
+          position: "top",
+        });
+        /* 새 글을 등록하면 홈으로 이동시킴 */
+        navigate("/");
+      })
+      .catch()
+      .finally();
   }
 
   return (
