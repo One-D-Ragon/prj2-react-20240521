a86a20c050fcc052d16fb757c78f3150227b5f92
1dragon <dnjsdyd159@naver.com>
2024-05-30 12:43:29 +0900

    댓글 전송 후 toast, 댓글 초기화
    useEffect 설명 추가

--- src/component/comment/CommentList.jsx
+++ src/component/comment/CommentList.jsx
@@ -16,6 +16,8 @@ export function CommentList({ boardId, isSending }) {
         .finally(() => {});
     }
   }, [isSending]); // isSending이 변경될 때 트리거시킴
+  // useEffect의 두번째 인자는 dependency로 인자로 받은 값의 상태가 바뀌었을 때 다시 트리거 한다
+  // 따라서 첫번째 인자값의 실행 상태가 두번째 인자에 들어가면 무한 반복이 될 수 있다? (강의 다시 참고)
   if (commentList.length === 0) {
     return <Box>댓글이 없습니다. 첫 댓글을 작성해보세요.</Box>;
   }
