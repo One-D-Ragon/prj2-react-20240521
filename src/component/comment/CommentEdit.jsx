3d7e8edfe1755a0906c5c3671a3670de53884c05
1dragon <dnjsdyd159@naver.com>
2024-05-31 11:23:28 +0900

    댓글 수정 전 모달, 수정 후 토스트

--- src/component/comment/CommentEdit.jsx
+++ src/component/comment/CommentEdit.jsx
@@ -1,4 +1,17 @@
-import { Box, Button, Flex, Textarea } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Textarea,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
@@ -12,6 +25,8 @@ export function CommentEdit({
   isProcessing,
 }) {
   const [commentText, setCommentText] = useState(comment.comment);
+  const { onClose, onOpen, isOpen } = useDisclosure();
+  const toast = useToast();
 
   function handleCommentSubmit() {
     setIsProcessing(true);
@@ -20,7 +35,13 @@ export function CommentEdit({
         id: comment.id,
         comment: commentText,
       })
-      .then(() => {})
+      .then(() => {
+        toast({
+          description: "댓글이 수정되었습니다.",
+          position: "top",
+          status: "success",
+        });
+      })
       .catch(() => {})
       .finally(() => {
         setIsProcessing(false);
@@ -40,19 +61,34 @@ export function CommentEdit({
         <Button
           variant="outline"
           colorScheme={"gray"}
-          onChange={() => setIsEditing(false)}
+          onClick={() => setIsEditing(false)}
         >
           <FontAwesomeIcon icon={faXmark} />
         </Button>
         <Button
           isLoading={isProcessing}
-          onClick={handleCommentSubmit}
+          onClick={onOpen}
           variant="outline"
           colorScheme={"blue"}
         >
           <FontAwesomeIcon icon={faPaperPlane} />
         </Button>
       </Box>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>수정 확인</ModalHeader>
+          <ModalBody>댓글을 저장하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Button colorScheme={"gray"} onClick={onClose}>
+              취소
+            </Button>
+            <Button colorScheme={"blue"} onClick={handleCommentSubmit}>
+              확인
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Flex>
   );
 }
