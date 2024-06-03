bccdb28848357717d2066d5ca7bf1469da5c63b9
1dragon <dnjsdyd159@naver.com>
2024-06-04 02:46:04 +0900

    main.jsx 설명 추가

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  // 개발 때만 react 컴포넌트를 붙였다 떼었다 붙이는 일을 한다
   <React.StrictMode>
     <App />
   </React.StrictMode>,
